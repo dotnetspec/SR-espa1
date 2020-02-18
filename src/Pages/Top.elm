f5947e861b3c3bdf57a9ac3a244b9fb50cca592d
prmockridge@gmail.com <FK8v4nxB>
2020-02-18 10:09:47 +0800

    add Top Dialog

--- src/Pages/Top.elm
+++ src/Pages/Top.elm
@@ -12,6 +12,7 @@ import Eth.Types
 import Eth.Utils
 import Ports
 import Eth.Net as Net exposing (NetworkId(..))
+import Dialog
 
 
 page : Page Params.Top Model Msg model msg appMsg
@@ -31,8 +32,31 @@ page =
 
 type alias Model =
     { account : Maybe Eth.Types.Address
-    , node : Ports.EthNode }
+    , node : Ports.EthNode
+    , user : User
+    , showDialog : Bool 
+    }
 
+type alias User = 
+    { 
+    account : Maybe Eth.Types.Address
+    , name : String
+    , description : String
+    , contact : String 
+    , email : String 
+        }
+
+type alias Config msg =
+    { closeMessage : Maybe msg
+    , maskAttributes : List (Attribute msg)
+    , containerAttributes : List (Attribute msg)
+    , headerAttributes : List (Attribute msg)
+    , bodyAttributes : List (Attribute msg)
+    , footerAttributes : List (Attribute msg)
+    , header : Maybe (Element msg)
+    , body : Maybe (Element msg)
+    , footer : Maybe (Element msg)
+    }
 
 init : Params.Top -> ( Model, Cmd Msg, Cmd Global.Msg )
 init _ =
@@ -43,7 +67,16 @@ init _ =
                 |> Ports.ethNode
     in
     ( { account = Nothing
-    , node = node }
+    , node = node
+    , user =  { 
+            account = Nothing
+            , name = ""
+            , description = ""
+            , contact = "" 
+            , email = "" 
+        } 
+    , showDialog = False
+    }
     , Cmd.none
     , Cmd.none
     )
@@ -57,6 +90,7 @@ type Msg
     = WalletStatus Eth.Sentry.Wallet.WalletSentry
     | Fail String
     | NoOp
+    | CloseDialog
 
 
 update : Msg -> Model -> ( Model, Cmd Msg, Cmd Global.Msg )
@@ -66,6 +100,13 @@ update msg model =
                             ( { model
                                 | account = walletSentry_.account
                                 , node = Ports.ethNode walletSentry_.networkId
+                            }
+                            , Cmd.none, Cmd.none
+                            )
+
+                CloseDialog ->
+                            ( { model
+                                | showDialog = False
                             }
                             , Cmd.none, Cmd.none
                             )
@@ -83,7 +124,7 @@ update msg model =
                             ( model, Cmd.none, Cmd.none )
            
 
---TODO: re-factor - repeated code - 
+--TODO: re-factor - repeated code - to MyUtils?
 validateAddress : Maybe Eth.Types.Address -> String
 validateAddress addr =
   case addr of
@@ -95,18 +136,39 @@ validateAddress addr =
 
 -- SUBSCRIPTIONS
 
-
--- subscriptions : Model -> Sub Msg
--- subscriptions model =
---     Sub.none
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
         Ports.walletSentry (Eth.Sentry.Wallet.decodeToMsg Fail WalletStatus)
+  
 
-
--- VIEW
 view : Utils.Spa.PageContext -> Model -> Element Msg
 view context model =
-    Ui.hero {  title = "SPORTRANK", description = "Context " ++ context.global.incomingData ++ validateAddress model.account, buttons = [ ( "Continue ...", "/rankings" ) ] }
-    --context.global.incomingData
+    let
+            config =
+                { closeMessage = Just CloseDialog
+                , maskAttributes = []
+                , containerAttributes = [ centerX, centerY, padding 10 ]
+                , headerAttributes = []
+                , bodyAttributes = []
+                , footerAttributes = []
+                , header = Just (text "Hello world")
+                , body = Just modalBody
+                , footer = Nothing
+                }
+
+            dialogConfig =
+                if model.showDialog then
+                    Just config
+
+                else
+                    Nothing
+        in
+    --Element.layout
+        
+        Element.el [ inFront (Dialog.view dialogConfig)] 
+        --(Element.text "hello")
+       (Ui.hero {  title = "SPORTRANK", description = "Context " ++ context.global.incomingData ++ validateAddress model.account, buttons = [ ( "Continue ...", "/rankings" ) ] })
+
+modalBody: Element Msg 
+modalBody = 
+    Element.text "hi there"
