4d8f365981aa98e3109a2db32a4c8c6e111c4b40
prmockridge@gmail.com <FK8v4nxB>
2020-07-23 16:46:19 +0800

    tidy

--- src/static/index.js
+++ src/static/index.js
@@ -15,24 +15,13 @@ const ethEnabled = () => {
   return false;
 }
 
-// const ethereumButton = document.querySelector('.enableEthereumButton');
-
-// ethereumButton.addEventListener('click', () => {
-//   //Will Start the metamask extension
-//   ethereum.request({ method: 'eth_requestAccounts' });
-// });
-
-// var app = Elm.Main.init({
-//     node: document.getElementById('elmapp')
-// });
-
-
 
 window.addEventListener('load', function () {
     if (typeof ethEnabled()) {
         console.log('MetaMask is installed!');
         console.log('Network version: ', window.ethereum.networkVersion);
         window.ethereum.autoRefreshOnNetworkChange = false;
+        //this window.ports.init step is currently necessary
         app = window.ports.init(Elm.Main.init({ flags: setOfFlags, node: node }));
     } 
     else {
@@ -45,23 +34,11 @@ window.addEventListener('load', function () {
 });
 
 
-
 //multi-port initialize
 window.ports = {
     init: (app) =>
         (
             //js is subscribed to Elm's 'outgoing' port:
-            // app.ports.outgoing.subscribe(({ action, data }) =>
-
-            //     actions[action]
-            //         ? actions[action](data)
-            //         : console.warn(`I didn't recognize action "${action}".`)
-            // )
-            
-            // app.ports.outgoing.subscribe(({ action, data }) =>
-            //     ethereum.request({ method: 'eth_requestAccounts' })
-            //}
-            //)
             app.ports.outgoing.subscribe(function({ action, data }) {
                 console.log(`From Elm at port:`, data)
                 if (data == "eth_requestAccounts") {
@@ -70,9 +47,6 @@ window.ports = {
                     console.log('Nothing happened');
                 }
             })
-
-            // next line commented to enable walletSentry until we're ready to implement txSentry
-            // ,
             // elm_ethereum_ports.txSentry(app.ports.txOut, app.ports.txIn, web3)
             // , elm_ethereum_ports.walletSentry(app.ports.walletSentry, web3)
             //,
@@ -80,34 +54,3 @@ window.ports = {
             , elm_ethereum_ports.walletSentry(app.ports.walletSentry, window.ethereum)
         )
 }
-
-
-// maps outgoing actions to functions!
-const actions = {
-    'LOG': (message) =>
-        console.log(`From Elm:`, message)
-}
-
-// Example code:
-// Create your WebSocket.
-// var socket = new WebSocket('wss://echo.websocket.org');
-
-// // When a command goes to the `sendMessage` port, we pass the message
-// // along to the WebSocket.
-// app.ports.sendMessage.subscribe(function(message) {
-//     socket.send(message);
-// });
-
-// // When a message comes into our WebSocket, we pass the message along
-// // to the `messageReceiver` port.
-// socket.addEventListener("message", function(event) {
-//     app.ports.messageReceiver.send(event.data);
-// });
-
-// const ethereumButton = document.querySelector('.enableEthereumButton');
-
-// ethereumButton.addEventListener('click', () => {
-//   //Will Start the metamask extension
-//   alert("here now")
-//   ethereum.request({ method: 'eth_requestAccounts' });
-// });
